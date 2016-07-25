558f4ce2826ba1c63b85119836b2809f9374997f
paultomchik <paultomchik@gmail.com>
2016-07-24 22:53:37 -0400

    Updated feedURLs.

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   feedURLs : [
     'http://siri.mta.availabs.org/api/siri/vehicle-monitoring.json', 
     'http://siri.mta.lline.availabs.org/api/siri/vehicle-monitoring.json', 
-    'http://siri.statenisland.availabs.org/api/siri/vehicle-monitoring.json', 
-    'http://siri.lirr.availabs.org/api/siri/vehicle-monitoring.json', 
+    'http://siri.mta.statenisland.availabs.org/api/siri/vehicle-monitoring.json', 
+    'http://siri.mta.lirr.availabs.org/api/siri/vehicle-monitoring.json', 
   ],
 }
